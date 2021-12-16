8a37f4d88b224fe63ed343ca577c0c1eadda1cd0
jeorp <jeorpqxx@gmail.com>
2021-12-16 15:49:42 +0900

    fix

---
+++ src/Redords.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE TemplateHaskell, DataKinds, TypeOperators, FlexibleContexts #-}
+{-# OPTIONS_GHC -fno-warn-unticked-promoted-constructors #-}
+
+module Records where
+
+import Data.Extensible
+import Control.Lens hiding ((:>))
