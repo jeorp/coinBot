280d97c35a3bb8d1f651f294e4890bf6be536e47
jeorp <jeorpqxx@gmail.com>
2022-01-01 00:22:03 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Gmo.ToRecord
 import Common
 
 main :: IO ()
-main = notateExample "https://discord.com/api/webhooks/913133402125905920/vqT0NGoszoS0jYkyFI1xcxzdpPCLVqRmM-n3uUKxvsDOhU_r33Q3UJ6cVe9L49RE4trg" BTC
+main = pure ()
 
 
 
