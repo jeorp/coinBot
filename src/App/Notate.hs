3abb5cb6f440ad403d3f37473f75eab4bfc3ac35
jeorp <jeorpqxx@gmail.com>
2022-01-01 03:44:51 +0900

    upd

--- src/App/Notate.hs
+++ src/App/Notate.hs
@@ -1,1 +1,26 @@
+
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings  #-}
 module App.Notate where
+
+import Common
+
+import Control.Lens
+import Control.Monad
+import Record
+import Gmo.ToRecord
+import Notate
+
+notateExample :: String -> Coin -> IO ()
+notateExample url c = extractRatesFromWs c exampleDo
+  where
+    exampleDo :: Maybe Rate -> IO ()
+    exampleDo (Just r) = discordHook url ("ask: " <> r ^. #ask <> ", bid: " <> r ^. #bid) >>= \t -> unless (t=="") $ print t
+    exampleDo _ = print "error"
+
+notateExample2 :: String -> Coin -> IO ()
+notateExample2 url c = extractOrderBooksFromWs c exampleDo2
+  where
+    exampleDo2 :: Maybe OrderBooks -> IO ()
+    exampleDo2 (Just ob) = discordHook url ("ask: " <> head (ob ^. #asks) ^. #price <> ", bid: " <> head (ob ^. #bids) ^. #price) >>= \t -> unless (t=="") $ print t
+    exampleDo2 _ = print "error"
--- src/Notate.hs
+++ src/Notate.hs
@@ -1,26 +1,19 @@
-{-# LANGUAGE TemplateHaskell, DataKinds, TypeOperators, FlexibleContexts #-}
 {-# LANGUAGE OverloadedLabels  #-}
 {-# LANGUAGE OverloadedStrings  #-}
-{-# OPTIONS_GHC -fno-warn-unticked-promoted-constructors #-}
 module Notate where
 
 import Common
 import Data.Maybe
 import           Data.Aeson               
-import qualified Data.Aeson.QQ              as Aeson.QQ
 import qualified Data.ByteString            as B
 import qualified Data.ByteString.Char8      as BS
 import qualified Data.ByteString.Lazy.Char8 as S8
-import           Data.Monoid                ((<>))
 import qualified Data.String                as S
 import qualified Data.Text as T
 import Data.Text.Encoding
 import           Data.Time.Clock.POSIX      (getPOSIXTime)
 import           Network.HTTP.Simple
-import Control.Lens
-import Control.Monad
-import Record
-import Gmo.ToRecord
+
 
 postJson :: S.String -> Value -> IO BS.ByteString 
 postJson url json = do
@@ -41,16 +34,3 @@ discordHook url msg =
         json = fromMaybe Null t
         in postJson url json
 
-notateExample :: String -> Coin -> IO ()
-notateExample url c = extractRatesFromWs c exampleDo
-  where
-    exampleDo :: Maybe Rate -> IO ()
-    exampleDo (Just r) = discordHook url ("ask: " <> r ^. #ask <> ", bid: " <> r ^. #bid) >>= \t -> unless (t=="") $ print t
-    exampleDo _ = print "error"
-
-notateExample2 :: String -> Coin -> IO ()
-notateExample2 url c = extractOrderBooksFromWs c exampleDo2
-  where
-    exampleDo2 :: Maybe OrderBooks -> IO ()
-    exampleDo2 (Just ob) = discordHook url ("ask: " <> head (ob ^. #asks) ^. #price <> ", bid: " <> head (ob ^. #bids) ^. #price) >>= \t -> unless (t=="") $ print t
-    exampleDo2 _ = print "error"
